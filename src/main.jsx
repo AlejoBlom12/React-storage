aa462f507c606f9a29c697b44f9d70e236b9c10c
AlejoBlom <alejandrogarzon197@gmail.com>
2024-04-08 14:34:03 -0500

    Intentando correguir el error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import { GitSuperApp } from './GitSuperApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <GitSuperApp />
-  </React.StrictMode>,
+  </>,
 )
